0bacdc705f14647c1ea1492204a3ea5ad20883fb
Deibyg <40776476+deiybg@users.noreply.github.com>
2026-01-24 01:16:20 +0100

    add favicon image

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
-let x = 0;
-for (let i = 0; i < 3; i++) {
-  x++;
-}
+const entrenos = ["Pecho", "Espalda", "Pecho", "Piernas", "Espalda", "Pecho"];
 
-console.log(x);
+const conteoGrupso = entrenos.reduce((acc, grupo) => {
+  // Si el grupo ya existe en el objeto, le suma 1; si no, lo crea en 0 y le suma 1.
+  acc[grupo] = (acc[grupo] || 0) + 1;
+  return acc;
+}, {}); // Iniciamos con un objeto vacío {}
+
+console.log(conteoGrupso);
+// { Pecho: 3, Espalda: 2, Piernas: 1 }
