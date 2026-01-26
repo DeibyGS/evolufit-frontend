bcc7b32a391b02fa247b173063f44e942333bcd0
Deibyg <40776476+deiybg@users.noreply.github.com>
2026-01-26 21:49:36 +0100

    fixed some bugs

--- src/api/API.js
+++ src/api/API.js
@@ -1,9 +1,4 @@
 /**
  * CONFIGURACIÓN CENTRAL DE LA API - EVOLUTFIT
  */
-
-// Eliminamos el /v1 para que coincida exactamente con tu servidor actual
-// src/api/API.js
-
-// FORZAR URL DE RENDER PARA PRUEBAS
 export const BASE_URL = "https://evolufit-backend.onrender.com/api";
